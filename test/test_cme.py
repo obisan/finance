40ce379eeda0d7775f81eba32df3aaf91db8734b
Dubinets Aleksandr <dubinets.av@gmail.com>
2023-08-12 11:46:35 +0300

    unittest: connection to cme ftp

--- test/test_cme.py
+++ test/test_cme.py
@@ -1,23 +1,26 @@
 import unittest
 
+from cme import CME
 from db import StorageDb
 from helpers.helpers import Helper
 from log import Logger
 from repository import RepositoryBulletin
-from scenario import DailybulletinSync
 
 
 class TestCME(unittest.TestCase):
     def setUp(self) -> None:
-        s = 0
+        pass
 
     def test01(self):
-        settings = Helper.get_settings('config_unittest_sync_bulletin.xml')
+        with open('config_unittest_sync_bulletin.xml', 'r') as file:
+            xml_path = file.read()
+            settings = Helper.get_settings(xml_path)
 
-        filename = settings['filename']
-        repository = settings['repository']
+            filename = settings['filename']
+            repository = settings['repository']
 
-        storage_db = StorageDb(filename=filename)
-        repository_bulletin = RepositoryBulletin(repository, Logger())
-        dailybulletin_sync = DailybulletinSync(storage_db, repository_bulletin, Logger())
-        dailybulletin_sync.sync()
+            storage_db = StorageDb(filename=filename)
+            repository_bulletin = RepositoryBulletin(repository, Logger())
+            cme = CME(storage_db=storage_db, repository=repository_bulletin)
+            dailybulletin_list = cme.get_dailybulletin_list()
+            self.assertNotEquals(dailybulletin_list, [], 'Error: Empty list from CME ftp.')
